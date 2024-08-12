0a101d3683aeed11241e3c535674d33bb097654a
Yulei Qiu <ray.yuleiqiu@gmail.com>
2024-08-12 11:25:17 +0100

    First commit

---
+++ post_script.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+apt-get update 
+DEBIAN_FRONTEND=noninteractive apt-get install -y keyboard-configuration
+DEBIAN_FRONTEND=noninteractive TZ="Europe/London" apt-get install -y tzdata
+apt-get -y upgrade
+
+apt-get install -y --force-yes \
+    wget \
+    tmux \
+    zip \
+    unzip \
+    git \
+    build-essential \
+    software-properties-common \
+    pypy \
+    cmake \
+    curl \
+    xterm \
+    psmisc \
+    software-properties-common \
+    apt-utils \
+    python3-pip \
+    libxext6 \
+    libx11-6 \
+    libglvnd0 \
+    libgl1 \
+    libglx0 \
+    libegl1 \
+    freeglut3-dev \
+    && apt-get -y autoremove \
+    && apt-get clean
+
+# Installation of NVidia Drivers and Cuda
+apt install -y --force-yes \
+    nvidia-driver-470 \
+    nvidia-cuda-toolkit \
+    && apt-get -y autoremove \
+    && apt-get clean
+
+cd /tmp
+wget https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/cuda_11.3.1_465.19.01_linux.run
+sh ./cuda_11.3.1_465.19.01_linux.run --toolkit --silent --override
+
+CUSTOM_ENV=/.singularity.d/env/99-zz_custom_env.sh
+cat >$CUSTOM_ENV <<EOF
+#!/bin/bash
+PS1="[lerobot] Singularity> \w \$ "
+EOF
+chmod 755 $CUSTOM_ENV
+
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# --contain without binding /dev was causing some strange CPU usage whenever MuJoCo
+# was running, two solutions found: not using --contain at all, or using --contain and
+# binding host's /dev to container.
+
+rm -rf workdir/*
+
+singularity run \
+    --contain \
+    $(if lspci | grep -qi nvidia; then echo ' --nv'; else echo ''; fi) \
+    $(if [ -f "${HOME}/.gitconfig" ]; then echo ' --bind='${HOME}'/.gitconfig'; else echo ''; fi) \
+    --bind=/etc/hosts \
+    --bind=/etc/localtime \
+    --workdir=workdir \
+    --bind=/proc \
+    --bind=/sys \
+    --bind=/dev \
+    --bind=scripts:/scripts \
+    --home=home:${HOME} \
+    --bind=/run/user/${UID} \
+    --bind=$HOME/.ssh \
+
